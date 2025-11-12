ffcb2e4949e68a26d4b4d4c4f4bdf464c274c076
will-augustyn <will.augustyn0404@gmail.com>
2025-11-11 19:32:03 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from experiments import particle_in_a_box, harmonic_oscillator
+
+# particle_in_a_box.main("gaussian", 'psi_x', True)
+
+harmonic_oscillator.main('gaussian', 'psi_x', save_name='QHO_psi_x.mp4')
+
+
+### meta materials 
+## 2-d atom 
+## adding measuremnt 
+# auto scaling parameters 
+# cleaning up animation code 
+# tqdm for progress bars 
